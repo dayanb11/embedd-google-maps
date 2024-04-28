b4d939781801701e9d740f48a5e210b6afa0aeb1
dayanb11 <dayanbeni@gmail.com>
2024-04-28 18:54:19 +0300

    need props for hidden

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import MyHome from './MyHome';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54063.02072212569!2d34.866222880611716!3d32.12495517017353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d30e372cbf191%3A0xd893416c5f47cba8!2sRevivim%20St%20129%2C%20Rosh%20Haayin!5e0!3m2!1sen!2sil!4v1714233879470!5m2!1sen!2sil" width="600" height="450" style={{"border": "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  );
-}
+  const [myBool=false, setmyBool] = useState(true);
+  function toggleBool() {
+    setmyBool(!myBool)
+  }
 
+  function DisplayMyHome(){
+    toggleBool();
+    alert('DisplayMyHome '+ myBool)
+  }
+
+ return(<div className='container_div'>
+  <div className='inner_div'>
+    <button className='Button' onClick={DisplayMyHome}>ShowMyHome</button>
+  </div>
+  <MyHome myBool></MyHome>
+  </div>);
+}
 export default App;
--- src/MyHome.jsx
+++ src/MyHome.jsx
@@ -3,7 +3,6 @@ function MyHome() {
   return (
     <div>
       <h1>My Home</h1>
-    <iframe title='MyHome' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54063.02072212569!2d34.866222880611716!3d32.12495517017353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d30e372cbf191%3A0xd893416c5f47cba8!2sRevivim%20St%20129%2C%20Rosh%20Haayin!5e0!3m2!1sen!2sil!4v1714233879470!5m2!1sen!2sil" width="600" height="450" style={{"border": "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  );
+        return(<iframe  title='MyHome' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54063.02072212569!2d34.866222880611716!3d32.12495517017353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d30e372cbf191%3A0xd893416c5f47cba8!2sRevivim%20St%20129%2C%20Rosh%20Haayin!5e0!3m2!1sen!2sil!4v1714233879470!5m2!1sen!2sil" width="600" height="450" style={{"border": "0"}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>);
 } export default MyHome;
